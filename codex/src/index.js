8ae4df842e570e1380ed71042c5346bba1c3dcc5
JSheleg <jessica.sheleg@gmail.com>
2022-01-18 15:31:39 -0600

    feat:add front matter to process and test

--- codex/src/index.js
+++ codex/src/index.js
@@ -1,12 +1,15 @@
 const unified = require('unified')
+//import {unified} from 'unified'
 const markdown = require('remark-parse')
-//const frontmatter = require('remark-frontmatter')
+//import markdown from 'remark-parse'
+const frontmatter = require('remark-frontmatter')
+//import remarkFrontmatter from 'remark-frontmatter'
 
 
 function process (markdownBuffer) {
-  const tree = unified().use(markdown).parse(markdownBuffer)
+  const tree = unified().use(markdown).use(frontmatter, ['yaml','toml']).parse(markdownBuffer)
 
-  //console.log(tree)
+  console.log(tree)
   return tree
 }
 
